c0008396fba777a998a70af559cb0a376fbc0cab
Jana Rashed <es.jana.rashad2023@alexu.edu.eg>
2025-12-14 10:39:03 +0200

    Fixed Errors In SystemData

--- System/SystemData.py
+++ System/SystemData.py
@@ -8,13 +8,12 @@ class SystemData:
     """
 
     def __init__(self, A: List[List[float]], b: List[float], method: str,
-                 precision: int, params: Dict[str, Any]):
+                 precision: int, params: Dict[str, Any], use_scaling: bool = False):
         self.A = A  # Coefficient Matrix
         self.b = b  # Constant Vector
         self.method = method  # Solving method
         self.precision = precision  # Number of significant figures
         self.params = params  # Method-specific parameters
         self.N = len(A)  # Size of the system
-        self.single_step = False #Single Step Mode Flag
-        self.params["use_scaling"] = False #Use Scaling Mode Flag
-
+        self.single_step = False  # Single Step Mode Flag
+        self.use_scaling = use_scaling  # Use Scaling Mode Flag (BONUS #3)
